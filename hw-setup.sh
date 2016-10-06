e72e31d8701095fb6f9cf88df50aa622edf9d788
Marcus Wanner <marcus@wanners.net>
2016-10-05 23:05:00 -0400

    Fix card setup script

--- hw-setup.sh
+++ hw-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 die () {
-    echo "$1" > &2
+    echo "$1" >&2
     exit -1
 }
 
-grep -Fxq "Revolution51" /proc/asound/card1/id || die "use Revolution51 PCI card or reconfigure in PiFmRds/hw-setup.sh"
+grep -Fxq "Revolution51" /proc/asound/card1/id || die "use Revolution51 PCI card in slot 1 or reconfigure in PiFmRds/hw-setup.sh and PiFmRds/rds_play"
 amixer -qD hw:1 sset "PCM Headphone" -- -15dB || die "failed to set audio level"
